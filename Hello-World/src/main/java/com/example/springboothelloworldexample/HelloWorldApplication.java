fe76fefb3afa70b240e016e11d9756fa0f21d27f
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2024-08-08 09:19:44 -0300

    Application Custom properties

--- Hello-World/src/main/java/com/example/springboothelloworldexample/HelloWorldApplication.java
+++ Hello-World/src/main/java/com/example/springboothelloworldexample/HelloWorldApplication.java
@@ -1,5 +1,7 @@
 package com.example.springboothelloworldexample;
 
+import com.example.springboothelloworldexample.config.ConfigProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
@@ -9,15 +11,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class HelloWorldApplication implements ApplicationRunner {
+public class HelloWorldApplication implements CommandLineRunner {
 
+    @Autowired
+    private ConfigProperties configProperties;
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
     }
 
+
     @Override
-    public void run(ApplicationArguments args) throws Exception {
-        System.out.println(" *** Show Arguments *** ");
-        Arrays.stream(args.getSourceArgs()).forEach(System.out::println);
+    public void run(String... args) throws Exception {
+        System.out.println("*** mail properties");
+        System.out.println("From: " + configProperties.getFrom());
+        System.out.println("To: " + configProperties.getTo());
+        System.out.println("Default object: " + configProperties.getDefaultObject());
     }
 }
