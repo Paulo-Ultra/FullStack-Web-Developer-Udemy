4f3bb3473f5b82f34de95b50364aafcba1ca97df
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2024-08-22 15:35:32 -0300

    Spring Test And Actuator

---
+++ library-management-system/src/test/java/com/example/librarymanagementsystem/BookServiceImplTest.java
@@ -0,0 +1,40 @@
+package com.example.librarymanagementsystem;
+
+import com.example.librarymanagementsystem.entity.Book;
+import com.example.librarymanagementsystem.repository.BookRepository;
+import com.example.librarymanagementsystem.service.BookService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import javax.transaction.Transactional;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@Transactional
+public class BookServiceImplTest {
+
+    @Autowired
+    private BookService bookService;
+
+    @MockBean
+    private BookRepository bookRepository;
+
+    @Test
+    public void addBookExpectCreated(){
+
+        Book book = Book.builder()
+                .name("Book 1")
+                .description("This is book 1")
+                .price(12.99)
+                .build();
+
+        bookService.createBook(book);
+
+        Mockito.verify(bookRepository, Mockito.times(1)).save(book);
+    }
+}
