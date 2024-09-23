6de2bd6abb42de4bc7b3c08d4c05a908c6288d69
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2024-09-23 12:01:42 -0300

    #Cross Origin Allowed *

--- javacorner-admin/src/main/java/com/javacorner/admin/web/UserRestController.java
+++ javacorner-admin/src/main/java/com/javacorner/admin/web/UserRestController.java
@@ -1,13 +1,11 @@
 package com.javacorner.admin.web;
 
 import com.javacorner.admin.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin("*")
 public class UserRestController {
 
     private final UserService userService;
