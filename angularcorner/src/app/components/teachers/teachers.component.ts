2bb047d98a944d50a90aa3b5174c6453f5ba7e71
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2024-10-01 15:56:09 -0300

    #Instructors Component

--- angularcorner/src/app/components/teachers/teachers.component.ts
+++ angularcorner/src/app/components/teachers/teachers.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {InstructorsService} from "../../services/instructors.service";
+import {catchError, Observable, throwError} from "rxjs";
+import {PageResponse} from "../../model/page.response.model";
+import {Instructor} from "../../model/instructor.model";
+import {EmailExistsValidator} from "../../validators/emailexists.validator";
+import {UsersService} from "../../services/users.service";
+import {CoursesService} from "../../services/courses.service";
+import {Course} from "../../model/course.model";
 
 @Component({
   selector: 'app-teachers',
@@ -10,17 +19,118 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 export class TeachersComponent implements OnInit {
 
-  constructor(private modalService: NgbModal) {}
+  searchFormGroup!: FormGroup;
+  instructorsFormGroup!: FormGroup;
+  currentPage: number = 0;
+  pageSize: number = 10;
+  errorMessage!: string;
+  pageInstructors!: Observable<PageResponse<Instructor>>;
+  submitted: boolean = false;
+  modalInstructor!: Instructor;
+  pageCourses$!: Observable<PageResponse<Course>>;
+  coursesCurrentPage: number = 0;
+  coursesPageSize: number = 5;
+  coursesErrorMessage!: string;
+
+  constructor(private modalService: NgbModal,
+              private fb: FormBuilder,
+              private instructorService: InstructorsService,
+              private userService: UsersService,
+              private courseService: CoursesService) {}
 
   ngOnInit(): void {
+    this.searchFormGroup = this.fb.group({
+      keyword: this.fb.control('')
+    })
+    this.instructorsFormGroup = this.fb.group({
+      firstName: ["", Validators.required],
+      lastName: ["", Validators.required],
+      summary: ["", Validators.required],
+      user: this.fb.group({
+        email: ["", [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")],
+        [EmailExistsValidator.validate(this.userService)]],
+        password: ["", Validators.required],
+      })
+    })
+    this.handleSearchInstructors();
   }
 
 
   getModal(content: any){
+    this.submitted = false;
     this.modalService.open(content, { size: 'xl' })
-    console.log("Hello world")
   }
 
 
+  handleSearchInstructors() {
+    let keyword = this.searchFormGroup.value.keyword;
+    this.pageInstructors = this.instructorService.searchInstructors(keyword, this.currentPage, this.pageSize)
+      .pipe(catchError(err => {
+        this.errorMessage = err.message;
+        return throwError(err);
+      }));
+  }
+
+  goToPage(page: number) {
+    this.currentPage = page;
+    this.handleSearchInstructors();
+  }
+
+  handleDeleInstructor(instructor: Instructor) {
+    let conf = confirm("Are you sure?");
+    if(!conf) return;
+    this.instructorService.deleteInstructor(instructor.instructorId).subscribe({
+      next: () => {
+        this.handleSearchInstructors();
+      },
+      error: err => {
+        alert(err.message);
+        console.log(err);
+      }
+    })
+  }
+
+  onCloseModal(modal: any) {
+    modal.close();
+    this.instructorsFormGroup.reset();
+  }
+
+  onSaveInstructor(modal: any) {
+    console.log(this.instructorsFormGroup)
+    this.submitted = true;
+    if(this.instructorsFormGroup.invalid) return;
+    this.instructorService.saveInstructor(this.instructorsFormGroup.value).subscribe({
+      next:() => {
+        alert("Sucess saving Instructor");
+        this.handleSearchInstructors();
+        this.instructorsFormGroup.reset();
+        this.submitted = false;
+        modal.close();
+      }, error: err => {
+        alert(err.message)
+        console.log(err);
+      }
+    })
+  }
+
+  getCoursesModal(instructor: Instructor, coursesContent: any) {
+    this.coursesCurrentPage = 0;
+    this.modalInstructor = instructor;
+    this.handleSearchCourses(instructor);
+    this.modalService.open(coursesContent, {size: 'xl'});
+  }
+
+  private handleSearchCourses(instructor: Instructor) {
+    this.pageCourses$ = this.courseService.getCoursesByInstructor(instructor.instructorId, this.coursesCurrentPage, this.coursesPageSize)
+      .pipe(catchError(err => {
+        this.coursesErrorMessage = err.message;
+        return throwError(err);
+      }))
+  }
+
+  goToCoursesPage(page: number) {
+    this.coursesCurrentPage = page;
+    this.handleSearchCourses(this.modalInstructor);
+  }
 }
 
--- angularcorner/src/app/services/instructors.service.ts
+++ angularcorner/src/app/services/instructors.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Instructor} from "../model/instructor.model";
 import {environment} from "../../environments/environment";
+import {PageResponse} from "../model/page.response.model";
 
 @Injectable({
   providedIn: 'root'
@@ -14,4 +15,18 @@ export class InstructorsService {
     public findAllInstructors(): Observable<Array<Instructor>> {
       return this.http.get<Array<Instructor>>(environment.backendHost + "/instructors/all");
   }
+
+  public searchInstructors(keyword: string, currentPage: number, pageSize: number): Observable<PageResponse<Instructor>> {
+    return this.http.get<PageResponse<Instructor>>(environment.backendHost + "/instructors?keyword=" + keyword
+      + "&page=" + currentPage + "&size=" + pageSize);
+  }
+
+  public deleteInstructor(instructorId: number) {
+    return this.http.delete(environment.backendHost+"/instructors/"+instructorId);
+  }
+
+  public saveInstructor(instructor: Instructor): Observable<Instructor> {
+    return this.http.post<Instructor>(environment.backendHost+"/instructors", instructor);
+  }
+
 }
